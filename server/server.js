cb2ce590e2f92eeeedf762780dddeeae7469861a
Sarah Ford <sford85@gmail.com>
2016-10-01 15:48:10 -0700

    finished cleaning server.js, checked that it still works

--- server/server.js
+++ server/server.js
@@ -13,11 +13,42 @@ const request = require('request-promise');
 const results = [];
 const set_parameters = {};
 
+
 app.use(express.static(path.join(__dirname, '../')));
 app.listen(3000);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// receives category and coordinates data from front end, makes yelp call
+// and returns response to front end
+app.post('/', function (req, res) {
+  const points = JSON.parse(req.body.points);
+  set_parameters.term = req.body.category;
+  set_parameters.categories = req.body.category;
+
+  // maps through data and makes a yelp call for each set of coordinates
+  Promise.map(points, function (item) {
+    const latLong = item.lat + ',' + item.lng;
+    set_parameters.ll = latLong;
+    return request_yelp(set_parameters).then(function (body) {
+      return JSON.parse(body);
+    });
+  }).then(function (yelpBody) {
+    yelpBody.forEach(function (item) {
+      results.push({
+        rating: item.businesses[0].rating_img_url,
+        name: item.businesses[0].name,
+        url: item.businesses[0].url,
+        location: JSON.stringify(item.businesses[0].location.coordinate),
+      })
+    });
+  // sends yelp results to front end
+  }).then(function () {
+    res.send(results);
+  }).catch(function(e) {
+    throw e;
+  });
+});
 
 // helper function that compiles all the Yelp parameters and makes Yelp call
 function request_yelp(set_parameters) {
@@ -52,31 +83,3 @@ function request_yelp(set_parameters) {
   const apiURL = url + '?' + paramURL;
   return request(apiURL);
 }
-
-// function that receives category and coordinates data from front end
-app.post('/', function (req, res) {
-  const points = JSON.parse(req.body.points);
-  set_parameters.term = req.body.category;
-  set_parameters.categories = req.body.category;
-
-  // maps through data and makes a yelp call for each set of coordinates
-  Promise.map(points, function (item) {
-    const latLong = item.lat + ',' + item.lng;
-    set_parameters.ll = latLong;
-    return request_yelp(set_parameters).then(function (body) {
-      return JSON.parse(body);
-    });
-  }).then(function (yelpBody) {
-    yelpBody.forEach(function (item) {
-      results.push({
-        rating: item.businesses[0].rating_img_url,
-        name: item.businesses[0].name,
-        url: item.businesses[0].url,
-        location: JSON.stringify(item.businesses[0].location.coordinate),
-      })
-    });
-  // sends yelp results back to front end
-  }).then(function () {
-    res.send(results);
-  });
-});
